19dd5b83757889c0a5464b07e752a58ffb5adfb0
Trespassing <108928321+S-Trespassing@users.noreply.github.com>
2025-05-06 22:48:56 +0800

    Update main.py
    添加顶栏

--- main.py
+++ main.py
@@ -7,19 +7,23 @@
 @Author  ：Trespassing
 @Date    ：2025/5/4 21:08 
 """
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import ( QFileDialog)
-from PySide6.QtCore import (QCoreApplication,
-    QMetaObject,  QRect,
-    QSize,  Qt)
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
 from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QLabel,
-    QLineEdit, QMainWindow, QPushButton,
-    QStatusBar, QWidget)
+                               QLineEdit, QMainWindow, QMenu, QMenuBar,
+                               QPushButton, QSizePolicy, QStatusBar, QWidget, QFileDialog)
 from win32com import client
 import re
 from pathlib import Path
 import os
 import pyautogui
+import webbrowser
 first_level = [
     '一、', '二、', '三、', '四、', '五、',
     '六、', '七、', '八、', '九、', '十、',
@@ -39,11 +43,22 @@ class Ui_MainWindow(object):
         MainWindow.resize(640, 400)
         MainWindow.setMinimumSize(QSize(640, 400))
         MainWindow.setMaximumSize(QSize(640, 400))
+        icon = QIcon()
+        icon.addFile(u"../../../OIP-C.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        MainWindow.setWindowIcon(icon)
+        self.action1 = QAction(MainWindow)
+        self.action1.setObjectName(u"action1")
+        self.action2 = QAction(MainWindow)
+        self.action2.setObjectName(u"action2")
+        self.action3 = QAction(MainWindow)
+        self.action3.setObjectName(u"action3")
+        self.action4 = QAction(MainWindow)
+        self.action4.setObjectName(u"action4")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.groupBox = QGroupBox(self.centralwidget)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(50, 40, 551, 301))
+        self.groupBox.setGeometry(QRect(40, 20, 551, 301))
         self.bt_dl = QPushButton(self.groupBox)
         self.bt_dl.setObjectName(u"bt_dl")
         self.bt_dl.setGeometry(QRect(230, 220, 91, 31))
@@ -79,6 +94,18 @@ class Ui_MainWindow(object):
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.menuBar = QMenuBar(MainWindow)
+        self.menuBar.setObjectName(u"menuBar")
+        self.menuBar.setGeometry(QRect(0, 0, 640, 33))
+        self.menuAbout = QMenu(self.menuBar)
+        self.menuAbout.setObjectName(u"menuAbout")
+        MainWindow.setMenuBar(self.menuBar)
+
+        self.menuBar.addAction(self.menuAbout.menuAction())
+        self.menuAbout.addAction(self.action1)
+        self.menuAbout.addAction(self.action2)
+        self.menuAbout.addAction(self.action3)
+        self.menuAbout.addAction(self.action4)
 
         self.retranslateUi(MainWindow)
 
@@ -87,13 +114,18 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u66f2Star\u516c\u6587\u5904\u7406demo", None))
+        self.action1.setText(QCoreApplication.translate("MainWindow", u"\u9047\u5230\u95ee\u9898?", None))
+        self.action2.setText(QCoreApplication.translate("MainWindow", u"\u5173\u4e8e\u672c\u9879\u76ee", None))
+        self.action3.setText(QCoreApplication.translate("MainWindow", u"\u9879\u76ee\u76f4\u8fbe", None))
+        self.action4.setText(QCoreApplication.translate("MainWindow", u"\u8054\u7cfb\u4f5c\u8005", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"\u66f2Star\u516c\u6587\u5904\u7406demo", None))
         self.bt_dl.setText(QCoreApplication.translate("MainWindow", u"\u5f00\u59cb\u5904\u7406", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u8f93\u5165\u6587\u4ef6\u8def\u5f84:", None))
         self.bt1.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6", None))
         self.bt2.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6\u5939", None))
         self.process.setText("")
-
+        self.menuAbout.setTitle(QCoreApplication.translate("MainWindow", u"\u66f4\u591a", None))
+    # retranslateUi
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -103,6 +135,10 @@ class MainWindow(QMainWindow):
         self.ui.bt2.clicked.connect(self.select_folder)
         self.ui.bt_dl.clicked.connect(self.start_to_del)
         self.path=""
+        self.ui.action1.triggered.connect(lambda:pyautogui.alert(text="您可以通过qq:2669807502联系作者或者到github提交issue\n项目地址:https://github.com/S-Trespassing/easy_QFNU_document\n期待您的宝贵意见~~~",title="遇到问题?"))
+        self.ui.action2.triggered.connect(lambda:pyautogui.alert(text="本项目的初衷是一键处理繁琐的公文格式,您可以对项目对项目进行各种二创~\n项目地址:https://github.com/S-Trespassing/easy_QFNU_document\n",title="关于项目"))
+        self.ui.action3.triggered.connect(lambda:webbrowser.open_new_tab("https://github.com/S-Trespassing/easy_QFNU_document"))
+        self.ui.action4.triggered.connect(lambda:webbrowser.open_new_tab("https://qm.qq.com/cgi-bin/qm/qr?k=qIaENQvu9-jWTr5x2NUC2s-jTupUokdk"))
     def show_process(self,i,all):
         i=int(i)
         all=int(all)
